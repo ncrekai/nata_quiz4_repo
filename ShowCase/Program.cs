f1ae14c8ac2e88e89103ccd4bf906465b77eca9b
Natalie Rekai <nrekai@my.centennialcollege.ca>
2025-04-09 13:26:34 -0400

    Issue#1 - Add display feature

--- ShowCase/Program.cs
+++ ShowCase/Program.cs
@@ -7,5 +7,7 @@ class Program
         Console.WriteLine("This is Quiz 4 for COMP367");
         Console.WriteLine("Prepared by Natalie Rekai - Student ID# 301484514");
         Console.WriteLine("April 9, 2025");
+        Console.WriteLine("******");
+        Console.WriteLine("Feature to display local data on web page added by Natalie Rekai");
     }
 }
