3f7d2830e72fde42a0d135b9284991b97e8a2f05
Natalie Rekai <nrekai@my.centennialcollege.ca>
2025-04-09 12:35:39 -0400

    Add project files.

---
+++ ShowCase/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("This is Quiz 4 for COMP367");
+        Console.WriteLine("Prepared by Natalie Rekai - Student ID# 301484514");
+        Console.WriteLine("April 9, 2025");
+    }
+}
